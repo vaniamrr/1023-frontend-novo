41c72aa8e006e74fe511295e0dcbd8f263d51217
Vânia Moreira <vania.santos3@estudante.ifms.edu.br>
2025-09-17 11:52:29 -0400

    Criando formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css'
-//useffect
-import { useEffect, useState } from 'react'
+//Useffect
+import { useEffect } from 'react'
+import { useState } from 'react'
 type ProdutoType = {
-  _id: string
-  nome: string
-  preco: number
-  urlfoto: string
+  _id: string,
+  nome: string,
+  preco: number,
+  urlfoto: string,
   descricao: string
 }
 
@@ -16,23 +16,44 @@ function App() {
     fetch('/api/produtos')
       .then((response) => response.json())
       .then((data) => setProdutos(data))
+      .catch((error) => console.error('Erro ao buscar produtos:', error));
   }, [])
+  function handleForm(event: React.FormEvent<HTMLFormElement>){
+    event.preventDefault()
+    const form = event.currentTarget
+    const formData = new FormData(form)
+    fetch('/api/produtos', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+  }
 
-  return (
+  return(
     <>
-    <div>tere</div>
-    {produtos.map((produto) => (
-      <div key={produto._id}>
-        <h2>{produto.nome}</h2>
-        <p>{produto.descricao}</p>
-        <p>R$ {produto.preco}</p>
-        <img src={produto.urlfoto} alt={produto.nome} width="200" />
+    <div>Cadastro de Produtos</div>
+    <form onSubmit={handleForm}>
+      <input type="text" name="nome" placeholder="Nome" /><br />
+      <input type="number" name="preco" placeholder="Preço" /><br />
+      <input type="text" name="urlfoto" placeholder="Url da Foto" /><br />
+      <textarea name="descricao" placeholder="Descrição"></textarea><br />
+      <button type="submit">Cadastrar</button>
+    </form>
+
+    <div>Lista de Produtos</div>
+
+    {produtos.map((produtos) =>(
+      <div key={produtos._id}>
+        <h2>{produtos.nome}</h2>
+        <p>{produtos.preco}</p>
+        <img src={produtos.urlfoto} alt={produtos.nome} width="200" />
+        <p>{produtos.descricao}</p>
       </div>
-    ))
-    }
+    ))}
     </>
   )
 }
-
 
 export default App
